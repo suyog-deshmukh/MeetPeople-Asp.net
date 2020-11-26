bdd1f5611af8734ed0381d93601ca6eb9bf95006
suyog <dsuyog2013@gmail.com>
2020-11-26 13:45:32 +0530

    Angular added

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -3,10 +3,10 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        
-        
-        
-        
+        public string UserName { get; set; }
+
+
+
+
     }
 }
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  users: any;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response
+    }, error => {console.log(error)})
+  }
+  title = 'client';
+}
